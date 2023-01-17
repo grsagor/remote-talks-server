242428ff9e52aa05212eb434a930b5f8e0645c37
anichu <anis.cse5.bu@gmail.com>
2023-01-17 19:00:38 +0600

    rearrange code

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const colors = require("colors");
+require("dotenv").config();
+const userRoutes = require("./routes/userRoutes");
 const port = 5000 || process.env.PORT;
 const app = express();
 
+console.log(process.env.PORT);
+// middleware
 app.use(cors());
 app.use(express.json());
+app.use("/api/user/", userRoutes);
 
-async function run(){
-    try{
-        app.get('/', (req,res) => {
-            res.send('Remote Talks is running');
-        });
-
-    }
-    finally{
-
-    }
-
-}
-run().catch(console.log);
-
+app.get("/", async (req, res) => {
+	res.send(`server is running on port ${port}`);
+});
 
 app.listen(port, () => {
-    console.log(`Remote Talks is running on port: ${port}`);
-})
+	console.log(`Remote Talks is running on port: ${port}`.red.bold.underline);
+});
